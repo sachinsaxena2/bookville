f05e466584601c72395bb75a729edeb57dd266cd
Sachin Saxena <sachin.saxena@mamenta.com>
2023-03-07 01:16:42 +0530

    updated

--- src/main/java/com/rent/bookville/controller/OrderConfirmationController.java
+++ src/main/java/com/rent/bookville/controller/OrderConfirmationController.java
@@ -1,8 +1,12 @@
 package com.rent.bookville.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class OrderConfirmationController {
 
-   // @GetMapping("/hello123")
+    @GetMapping("/hello123")
     public String helloWorld() {
         String best = "perfect";
         return best;
