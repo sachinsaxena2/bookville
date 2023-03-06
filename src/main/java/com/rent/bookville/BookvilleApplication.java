d9fff65889f3ac3d4dc93b5b6c50ccbc912b787b
Sachin Saxena <sachin.saxena@mamenta.com>
2023-03-07 00:36:28 +0530

    updated

---
+++ src/main/java/com/rent/bookville/BookvilleApplication.java
@@ -0,0 +1,13 @@
+package com.rent.bookville;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookvilleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookvilleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rent/bookville/BookvilleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rent.bookville;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookvilleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
