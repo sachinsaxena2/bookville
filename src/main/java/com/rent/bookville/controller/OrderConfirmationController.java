8c5b1f1aba615bb582cf22b88d94c70a5cf39ef2
Sachin Saxena <sachin.saxena@mamenta.com>
2023-03-07 14:43:58 +0530

    updated

--- src/main/java/com/rent/bookville/controller/OrderConfirmationController.java
+++ src/main/java/com/rent/bookville/controller/OrderConfirmationController.java
@@ -1,13 +1,23 @@
 package com.rent.bookville.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class OrderConfirmationController {
 
     @GetMapping("/hello123")
     public String helloWorld() {
+        String best = "perfect";
+        return best;
+    }
+
+    @GetMapping("/confirmOrder")
+    public String saveOrder(@RequestParam String phoneNumber, @RequestParam(required = false) String email) {
+        log.info("PHONE NUMBER "+phoneNumber);
         String best = "perfect";
         return best;
     }
